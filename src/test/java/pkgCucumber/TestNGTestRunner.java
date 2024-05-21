177f8e153b605842854e05ebefce820b91419a08
Abhijnan <abhijnan.kundu@gmail.com>
2024-05-21 15:28:28 -0700

    0521-2

--- src/test/java/pkgCucumber/TestNGTestRunner.java
+++ src/test/java/pkgCucumber/TestNGTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features="src/test/java/pkgCucumber",
 glue={"pkgStepDefinitions","pkgHooks"},
 monochrome=true,
-tags="not @hold",
+tags="@tag2",
 plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","html:target/cucumber.html"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
